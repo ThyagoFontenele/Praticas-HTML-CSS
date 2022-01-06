ba7fa763f7cf09200e8eb613119f4374ffcb6208
thyago_fontenele <thyago.fontenele@injunior.com.br>
2022-01-06 04:11:47 -0300

    melhorando o header responsivo

--- Site Restaurante/script.js
+++ Site Restaurante/script.js
@@ -1,39 +1,12 @@
-function sizeOfThings(){
-    var windowWidth = window.innerWidth;
-    const menuIcon = document.querySelector('.menu-icon');
-    const menuHeader = document.querySelector('.header-list');
-    menuIcon.style.display = 'none';
-    if(windowWidth < 760){
-        menuIcon.style.display = '';
-        menuHeader.style.display = 'none';
-    }
-    else{
-        menuHeader.style.display = '';
-        const menu = document.querySelector('.menu');
-        const imgMenu = document.querySelector('.menu-icon');
-        imgMenu.src = './assets/menu.png';
-        menu.style.display = 'none'
-    }
-};
-sizeOfThings();
+const buttonMobile = document.querySelector('#btn-mobile');
 
-window.addEventListener('resize', function(){
-	sizeOfThings();
-});
+const toggleMenu = (event) => {
 
+    if(event.type === 'touchstart') { event.preventDefault(); };
+    const nav = document.querySelector('#nav');
+    nav.classList.toggle('active');
 
-const menu = document.querySelector('.menu');
-const imgMenu = document.querySelector('.menu-icon');
-menu.style.display = 'none'
-const handleMenu = () => {
-    if(menu.style.display == 'none'){
-        menu.style.display = ''
-        imgMenu.src = './assets/cancel.png';
-        imgMenu.style.width = '25px';
-    }else{
-        menu.style.display = 'none';
-        imgMenu.src = './assets/menu.png';
-        imgMenu.style.width = '35px';
-    }
-    
 }
+
+buttonMobile.addEventListener('click', toggleMenu);
+buttonMobile.addEventListener('touchstart', toggleMenu);
