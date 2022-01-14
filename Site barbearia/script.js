c432991459ce94c548df97a0cdece1d2075cf5d2
thyago_fontenele <thyago.fontenele@injunior.com.br>
2022-01-14 07:33:27 -0300

    aplicando menu hamburger responsivo

--- Site barbearia/script.js
+++ Site barbearia/script.js
@@ -1,11 +1,21 @@
 const scrollListener = () => {
-
+    const title = document.querySelector('.title');
+    title.style.marginTop = `${window.scrollY}px`;
     let bgHeader = document.querySelector('.bgHeader');
-    if(window.scrollY > 20){
-        bgHeader.style.backgroundColor = '#111';
+    if(window.scrollY > 600){
+        bgHeader.style.backgroundColor = '#c7c7c7';
     }else{
         bgHeader.style.backgroundColor = '';
     }
 
 }
 window.addEventListener('scroll', scrollListener);
+
+function toggleMenu(event) {
+    if(event.type === 'touchstart'){ event.preventDefault(); }
+    const nav = document.querySelector('#nav');
+    nav.classList.toggle('active');
+}
+const button = document.querySelector('#button');
+button.addEventListener('click', toggleMenu);
+button.addEventListener('touchstart', toggleMenu);
